b20be7acf6c78e72d7a826cedd087dc75eb1f79c
Tanish702 <136959853+Tanish702@users.noreply.github.com>
2023-06-21 11:15:11 +0530

    Update contact_book.py

--- contact_book.py
+++ contact_book.py
@@ -10,15 +10,8 @@ root.config(bg = '#d3f3f5')
 root.title('PythonGeeks Contact Book')
 root.resizable(0,0)
 contactlist = [
-    ['Siddharth Nigam','369854712'],
-    ['Gaurav Patil', '521155222'],
-    ['Abhishek Nikam', '78945614'],
-    ['Sakshi Gaikwad', '58745246'],
-    ['Mohit Paul', '5846975'],
-    ['Karan Patel', '5647892'],
-    ['Sam Sharma', '89685320'],
-    ['John Maheshwari', '98564785'],
-    ['Ganesh Pawar','85967412']
+    ['Tanish Chaudhary,'369854712'],
+    ['vinit Tiwari, '521155222']
     ]
 
 Name = StringVar()
